ad1a4ffd3dd4a5fc52312f7a34f815922a0a2df4
Nipun22 <nipunkataria98@gmail.com>
2019-02-08 17:26:48 -0800

    set up start script and heroku port

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 8080;
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -54,4 +55,6 @@ app.get('/bad', (req,res)=>{
         errorMessage: 'unable to process your request'
     });
 });
-app.listen(8080);
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
+});
